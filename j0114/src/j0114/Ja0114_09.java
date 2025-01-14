2684759dccbd4d27f739205c4800d47ff57d94fd
lim <mylim52@naver.com>
2025-01-14 18:25:41 +0900

    0114 조건,반복문

---
+++ j0114/src/j0114/Ja0114_09.java
@@ -0,0 +1,19 @@
+package j0114;
+
+public class Ja0114_09 {
+	public static void main(String[] args) {
+		
+		int score1 = 0;
+		int score2 = 1;
+		int score3 = 2,score4 = 3,score5 = 4;
+		
+		// 배열(Array)
+		int[] score = new int[5];
+		score[0] = 0;
+		score[1] = 1;
+		score[2] = 2;
+		score[3] = 3;
+		score[4] = 4;
+		
+	}
+}
